5d8667ab4a2cfbfd512af70d9cc0b143c27dbabd
Jethnithi Techasatian <bagpigham@gmail.com>
2017-09-01 19:24:38 +0700

    print key

--- line/in_event/crypto_bot.py
+++ line/in_event/crypto_bot.py
@@ -101,6 +101,7 @@ class CryptoBot:
                 global_price = float(self.cryptocurrencies[key].global_price)
                 bx_price = float(self.cryptocurrencies[key].bx_price)
                 gap = max(global_price, bx_price) / min(global_price, bx_price)
+                print(key)
                 if gap > 0.1:
                     if self.alert[key] % 10 == 0:
                         output = ""
