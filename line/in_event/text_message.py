70c233dc9e496daed464498b3924f6edbeaddbca
Jethnithi Techasatian <bagpigham@gmail.com>
2017-09-01 18:10:58 +0700

    push

--- line/in_event/text_message.py
+++ line/in_event/text_message.py
@@ -10,7 +10,7 @@ class TextMessage(object):
         text_inp = event.message.text.lower()
         #send_text.push(event.source.user_id, text_inp)
         print(event)
-        send_text.push(event.room_id, text_inp)
+        send_text.push(event.source.room_id, text_inp)
         send_text.push('R5a8df70a7425c3c8b60204f8176dcbcc', text_inp)
         send_text.reply(event.reply_token, 'ควยปั้น')
         #print('in_event')
