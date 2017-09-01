c4ef0459323bb029e0d8875d890173fb2114eb6a
Jethnithi Techasatian <bagpigham@gmail.com>
2017-09-02 01:42:24 +0700

    init

--- apis/__init__.py
+++ apis/__init__.py
@@ -1,7 +1,7 @@
 from flask_restplus import Api
 
 from .line_webhook import api as line_webhook
-
+from line.in_event import CryptoBot
 api = Api(
     title='Apis',
     version='1.0',
@@ -9,3 +9,6 @@ api = Api(
     # All API metadatas
 )
 api.add_namespace(line_webhook)
+currencies = ["BTC", "ETH", "DAS", "OMG", "XRP"]
+cryptoBot = CryptoBot(currencies)
+print('cryptoBot created ja')
--- line/in_event/text_message.py
+++ line/in_event/text_message.py
@@ -1,13 +1,13 @@
 import line.out_event
-from .crypto_bot import CryptoBot
+#from .crypto_bot import CryptoBot
 send_text = line.out_event.TextMessage()
-
 
 
 class TextMessage(object):
     def __init__(self):
-        self.currencies = ["BTC", "ETH", "DAS", "OMG", "XRP"]
-        self.cryptoBot = CryptoBot(self.currencies)
+        # self.currencies = ["BTC", "ETH", "DAS", "OMG", "XRP"]
+        # self.cryptoBot = CryptoBot(self.currencies)
+
         print('textMessage been init!!!')
     def core(self, event):
         #nput = event.message.text
