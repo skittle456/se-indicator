b35a1a6909e008bc0fbd6e56880739cad17375d0
Parin Kobboon <punparin@gmail.com>
2017-09-02 16:35:37 +0700

    Update

--- line/in_event/text_message.py
+++ line/in_event/text_message.py
@@ -8,8 +8,8 @@ class TextMessage(object):
         self.cryptoBot = CryptoBot(self.currencies)
 
     def core(self, event):
-        print(event.type)
-        if event.type == 'group':
+        print(event.source.type)
+        if event.source.type == 'group':
             self.cryptoBot.command(event.message.text, event.source.group_id)
         else:
             self.cryptoBot.command(event.message.text, event.source.user_id)
