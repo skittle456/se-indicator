518df30d7fdb669e83917159cce7afcd1a5040cc
Parin Kobboon <punparin@gmail.com>
2017-09-02 16:51:27 +0700

    Update

--- line/in_event/crypto_currency.py
+++ line/in_event/crypto_currency.py
@@ -9,11 +9,9 @@ class CryptoCurrency:
         self.percent_change_1h = None
         self.percent_change_1d = None
         self.percent_change_7d = None
-        self.time = strftime("%Y-%m-%d %H:%M:%S", gmtime(-1))
 
     def updateBX(self, bx_price):
         self.bx_price = bx_price
-        self.time = strftime("%Y-%m-%d %H:%M:%S", gmtime(-1))
 
     def updateGlobal(self, global_price, usd_price, percent_change_1h, percent_change_1d, percent_change_7d):
         self.global_price = global_price
@@ -21,7 +19,6 @@ class CryptoCurrency:
         self.percent_change_1h = percent_change_1h
         self.percent_change_1d = percent_change_1d
         self.percent_change_7d = percent_change_7d
-        self.time = strftime("%Y-%m-%d %H:%M:%S", gmtime(-1))
 
     def __str__(self):
-        return '%s | %s\n--------------------------------------\nGlobal Price: %s\nBX Price: %s\nUSD Price: %s\nPercent Change 1HR: %s\nPercent Change 1D: %s\nPercent Change 7D: %s\n' % (self.name, self.time, format(float(self.global_price), ".2f"), self.bx_price, self.usd_price, self.percent_change_1h, self.percent_change_1d, self.percent_change_7d)
+        return '%s\n--------------------------------------\nGlobal Price: %s\nBX Price: %s\nUSD Price: %s\nPercent Change 1HR: %s\nPercent Change 1D: %s\nPercent Change 7D: %s\n' % (self.name, format(float(self.global_price), ".2f"), self.bx_price, self.usd_price, self.percent_change_1h, self.percent_change_1d, self.percent_change_7d)
