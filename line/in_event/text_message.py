6215756225e1ec636b3ec422b02ee699a75e03c4
Parin Kobboon <punparin@gmail.com>
2017-09-04 15:26:24 +0700

    Fix bug

--- line/in_event/text_message.py
+++ line/in_event/text_message.py
@@ -2,10 +2,17 @@ import line.out_event
 from .crypto_bot import CryptoBot
 send_text = line.out_event.TextMessage()
 
+currencies = ["BTC", "ETH", "DAS", "OMG", "XRP"]
+instance = None
+
 class TextMessage(object):
     def __init__(self):
-        self.currencies = ["BTC", "ETH", "DAS", "OMG", "XRP"]
-        self.cryptoBot = CryptoBot(self.currencies)
+        global instance
+        if instance is None:
+            self.cryptoBot = CryptoBot(currencies)
+            instance = self.cryptoBot
+        else:
+            self.cryptoBot = instance
 
     def core(self, event):
         if event.source.type == "group":
